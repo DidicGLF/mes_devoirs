07a22a7c9bdbd1cb42ad5b135d1c542c7815c061
Didic <didic@ik.me>
2025-06-18 16:33:55 +0200

    Restructuration

--- lib/saisie.dart
+++ lib/saisie.dart
@@ -1,15 +1,13 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class Saisie extends StatefulWidget{
+class Saisie extends StatefulWidget {
   @override
   _saisieState createState() => _saisieState();
 }
 
 class _saisieState extends State<Saisie> {
-     //const Saisie({super.key});  
+  //const Saisie({super.key});
   Color _color = Colors.transparent;
-   TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -19,47 +17,119 @@ class _saisieState extends State<Saisie> {
         child: Column(
           spacing: 20,
           children: [
-           Container(
-              padding: EdgeInsets.all(10),              
+            Container(
+              padding: EdgeInsets.all(10),
               child: Row(
                 children: [
                   Spacer(),
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      _color = Colors.blue;
-                    });
-                  }, style: ElevatedButton.styleFrom(fixedSize: const Size(100, 70), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)), foregroundColor: Colors.black, backgroundColor: Colors.blue, textStyle: TextStyle(fontSize: 14)), child: Text("Classe 1")),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _color = Colors.blue;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 70),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.blue,
+                      textStyle: TextStyle(fontSize: 14),
+                    ),
+                    child: Text("Classe 1"),
+                  ),
                   Spacer(flex: 1),
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      _color = Colors.green;
-                    });
-                  }, style: ElevatedButton.styleFrom(fixedSize: const Size(100, 70), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)), foregroundColor: Colors.black, backgroundColor: Colors.green, textStyle: TextStyle(fontSize: 14)), child: Text("Classe 2")),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _color = Colors.green;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 70),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.green,
+                      textStyle: TextStyle(fontSize: 14),
+                    ),
+                    child: Text("Classe 2"),
+                  ),
                   Spacer(flex: 1),
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      _color = Colors.red;
-                    });
-                  }, style: ElevatedButton.styleFrom(fixedSize: const Size(100, 70), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)), foregroundColor: Colors.black, backgroundColor: Colors.red, textStyle: TextStyle(fontSize: 14)), child: Text("Classe 3")),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _color = Colors.red;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 70),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.red,
+                      textStyle: TextStyle(fontSize: 14),
+                    ),
+                    child: Text("Classe 3"),
+                  ),
                   Spacer(flex: 1),
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      _color = Colors.yellow;
-                    });
-                  }, style: ElevatedButton.styleFrom(fixedSize: const Size(100, 70), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)), foregroundColor: Colors.black, backgroundColor: Colors.yellow, textStyle: TextStyle(fontSize: 14)), child: Text("Classe 4")),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _color = Colors.yellow;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 70),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.yellow,
+                      textStyle: TextStyle(fontSize: 14),
+                    ),
+                    child: Text("Classe 4"),
+                  ),
                   Spacer(flex: 1),
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      _color = Colors.pink;
-                    });
-                  }, style: ElevatedButton.styleFrom(fixedSize: const Size(100, 70), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)), foregroundColor: Colors.black, backgroundColor: Colors.pink, textStyle: TextStyle(fontSize: 14)), child: Text("Classe 5")),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _color = Colors.pink;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 70),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.pink,
+                      textStyle: TextStyle(fontSize: 14),
+                    ),
+                    child: Text("Classe 5"),
+                  ),
                   Spacer(flex: 1),
-                  ElevatedButton(onPressed: (){
-                    setState(() {
-                      _color = Colors.purple;
-                    });
-                  }, style: ElevatedButton.styleFrom(fixedSize: const Size(100, 70), shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)), foregroundColor: Colors.black, backgroundColor: Colors.purple, textStyle: TextStyle(fontSize: 14)), child: Text("Classe 6")),
-                  Spacer()
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _color = Colors.purple;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 70),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.purple,
+                      textStyle: TextStyle(fontSize: 14),
+                    ),
+                    child: Text("Classe 6"),
+                  ),
+                  Spacer(),
                 ],
               ),
             ),
@@ -68,9 +138,9 @@ class _saisieState extends State<Saisie> {
             Container(
               decoration: BoxDecoration(
                 borderRadius: const BorderRadius.all(Radius.circular(9)),
-                color: _color,//const Color.fromRGBO(158, 158, 158, 1),
+                color: _color, //const Color.fromRGBO(158, 158, 158, 1),
               ),
-              padding: EdgeInsets.all(10), 
+              padding: EdgeInsets.all(10),
               margin: EdgeInsets.only(right: 20, left: 20),
               child: Row(
                 children: [
@@ -85,16 +155,20 @@ class _saisieState extends State<Saisie> {
                   Spacer(),
                   Text("Contenu"),
                   Spacer(),
-                  ElevatedButton(onPressed: (){}, child: Text("Valider")),
-                  Spacer()
+                  ElevatedButton(onPressed: () {}, child: Text("Valider")),
+                  Spacer(),
                 ],
               ),
             ),
-            
 
             //Liste des devoirs
             Container(
               decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue.shade400, Colors.blue.shade100],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
                 borderRadius: const BorderRadius.all(Radius.circular(9)),
                 border: Border.all(color: Colors.blue, width: 2),
                 color: const Color.fromARGB(255, 219, 213, 213),
@@ -108,22 +182,29 @@ class _saisieState extends State<Saisie> {
                   Spacer(flex: 1),
                   Text("Lun 4 nov"),
                   Spacer(flex: 1),
-                  Text("DS n°2 : Calcul littéral, transformation, puissances, exposant négatif, scratch en F303"),
+                  Text(
+                    "DS n°2 : Calcul littéral, transformation, puissances, exposant négatif, scratch en F303",
+                  ),
                   Spacer(flex: 1),
                   Text("3°1"),
                   Spacer(flex: 1),
-                  Checkbox(value: true, onChanged: (bool? value) {  },),
+                  Checkbox(value: true, onChanged: (bool? value) {}),
                   Spacer(flex: 1),
                   Icon(Icons.edit),
                   Spacer(flex: 1),
                   Icon(Icons.delete),
-                  Spacer()
+                  Spacer(),
                 ],
               ),
             ),
 
             Container(
               decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.green.shade400, Colors.green.shade100],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
                 borderRadius: const BorderRadius.all(Radius.circular(9)),
                 border: Border.all(color: Colors.green, width: 2),
                 color: const Color.fromARGB(255, 219, 213, 213),
@@ -137,22 +218,24 @@ class _saisieState extends State<Saisie> {
                   Spacer(flex: 1),
                   Text("Lun 4 nov"),
                   Spacer(flex: 1),
-                  Text("DS n°2 : Calcul littéral, transformation, puissances, exposant négatif, scratch en F303"),
+                  Text(
+                    "DS n°2 : Calcul littéral, transformation, puissances, exposant négatif, scratch en F303",
+                  ),
                   Spacer(flex: 1),
                   Text("3°1"),
                   Spacer(flex: 1),
-                  Checkbox(value: true, onChanged: (bool? value) {  },),
+                  Checkbox(value: true, onChanged: (bool? value) {}),
                   Spacer(flex: 1),
                   Icon(Icons.edit),
                   Spacer(flex: 1),
                   Icon(Icons.delete),
-                  Spacer()
+                  Spacer(),
                 ],
               ),
             ),
-        ],
+          ],
         ),
-        ),
+      ),
     );
   }
 }
